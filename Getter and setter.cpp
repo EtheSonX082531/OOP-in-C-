27b4e0a1caaf7f474dc8b93207038d6413efd203
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2024-12-28 21:13:21 +0600

    Create Getter and setter.cpp

---
+++ Getter and setter.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Student {
+	//default access modifier in C++ is private
+    string name;
+    int age;
+    int cls;
+    
+    public:
+    void setName(string n){
+    name=n;
+    }
+    
+    string getName(){
+     return name;
+     }
+};
+
+
+
+int main()
+{
+    Student s1;
+    s1.setName("Ayon Roy");
+    cout << s1.getName() << endl;
+    return 0;
+}
